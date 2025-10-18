290dc4479cfdbacd1a7737b8332e6f8e8d3339df
Ayran <ayranandrade@hotmail.com>
2025-10-18 19:45:31 -0300

    Add Express support for JSON

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,9 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', PropertyRouter);
 
 export default app;
